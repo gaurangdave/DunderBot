8d0f705743e3cabed537c53e80208b684a8f53cd
Gaurang Dave <gaurangdave@Jarvis.local>
2025-04-28 12:23:06 -0700

    added PWA support just so that I don't accidently close it while testing

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -141,4 +141,16 @@ document.addEventListener('DOMContentLoaded', function() {
             sendButton.click();
         }
     });
+
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/static/js/service-worker.js')
+                .then(registration => {
+                    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                })
+                .catch(error => {
+                    console.log('ServiceWorker registration failed: ', error);
+                });
+        });
+    }
 });
